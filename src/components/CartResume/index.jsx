88d0fb8f8ea0cac15490bf026a51098ca1be6b50
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2024-01-16 21:37:30 -0300

    testando

--- src/components/CartResume/index.jsx
+++ src/components/CartResume/index.jsx
@@ -37,7 +37,7 @@ export const CartResume = () =>{
             
             window.location.reload(true);
             localStorage.removeItem('codeburger:cartInfo');
-        }, 4100);
+        }, 4030);
 
     }
 
